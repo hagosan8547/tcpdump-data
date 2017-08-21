3c45b78e27bc163280a60d63469da961831434b3
hagosan <hagosn00001@gmail.com>
2017-08-21 22:21:16 +0900

    proxy handler 2

--- src/tcpdump-data.c
+++ src/tcpdump-data.c
@@ -24,15 +24,16 @@ int main(int argc, char const *argv[]) {
 
   switch (DEBUG_VALUE) {
     case DEBUG_MY_PACKET_HANDLER:
-
+    proxy_handler=my_packet_handler;
     break;
+
     case DEBUG_DATA_OUT_HANDLER:
-    
+    proxy_handler=data_out_handler;
     break;
   }
 
   handle = pcap_open_live(device, snapshot_length, 0, 10000, error_buffer);
-  pcap_loop(handle, total_packet_count, my_packet_handler, my_arguments);
+  pcap_loop(handle, total_packet_count, proxy_handler, my_arguments);
 
   return 0;
 }
